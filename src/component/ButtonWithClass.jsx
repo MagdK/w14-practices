66742dce830d944a4926cdf30b6e9719a9726f27
MagdK <kovacsmagda@gmail.com>
2022-04-20 18:07:54 +0200

    Remove some comments

--- src/component/ButtonWithClass.jsx
+++ src/component/ButtonWithClass.jsx
@@ -23,18 +23,4 @@ class ButtonWithClass extends Component {
     }
 }
 
-// function ButtonWithHooks() {
-//     const [key1, setKey1] = useState("This is the default state of a Function component.");
-//     const [key2, setKey2] = useState(0);
-
-//     return (
-//         <>
-//             <div>{key1}</div>
-//             <div>{key2}</div>
-//             <button onClick={() => setKey1("Function Button Clicked")}>Function Button</button>
-//             <button onClick={() => setKey2(1)}>Change the number</button>
-//         </>
-//     )
-// };
-
 export default ButtonWithClass;
